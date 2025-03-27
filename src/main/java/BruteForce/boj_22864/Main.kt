a59cb407aab18dfa128568c1258432dd1e2ecd2f
bhyunnie <dev.bhyunnie@gmail.com>
2025-03-28 03:30:46 +0900

    fix: destructuring 적용

--- src/main/java/BruteForce/boj_22864/Main.kt
+++ src/main/java/BruteForce/boj_22864/Main.kt
@@ -7,11 +7,7 @@ import kotlin.math.max
 // 20752kb 108ms
 fun main(args: Array<String>) {
 	val br = BufferedReader(InputStreamReader(System.`in`))
-	val arr = br.readLine().split(" ").map {it.toInt()}
-	val damage = arr[0]
-	val work = arr[1]
-	val heal = arr[2]
-	val deadline = arr[3]
+	val (damage,work,heal,deadline) = br.readLine().split(" ").map {it.toInt()}
 	var damageSum = 0
 	var result = 0
 	for (i in 0..23) {
